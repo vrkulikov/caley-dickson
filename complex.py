f40e006932600f396f0c62b2e034879906b76a29
vrkulikov <v.r.kulikov@mail.ru>
2021-10-16 13:03:39 +0700

    Ввод любого количествоа аргументов

--- complex.py
+++ complex.py
@@ -1,3 +1,5 @@
+from itertools import zip_longest
+
 def _conj(x):
     try:
         return x.conj
@@ -5,18 +7,68 @@ def _conj(x):
         return x
 
 class Complex:
-    def __init__(self,re=0, im=0) -> None:
-        self.re = re
-        self.im = im
+    def __init__(self,*args) -> None:
+        if len(args) == 0:
+            self.re = 0
+            self.im = 0
+        elif len(args)== 1:
+            self.re = args[0]
+            self.im = 0
+        elif len(args)==2:
+            self.re, self.im = args
+        else:
+            l = len(args)
+            k = 1
+            while k<l:
+                k*=2
+            new_args = args+tuple(0 for _ in range(k-l))
+            self.re=Complex(*new_args[:k//2])
+            self.im=Complex(*new_args[k//2:])
+    
+
+    @property
+    def order(self):
+        n = 0
+        a = self
+        while isinstance(a, Complex):
+            n += 1
+            a = a.re
+        return n
 
     def __repr__(self) -> str:
         return 'Complex({}, {})'.format(self.re,self.im)
+
+
+    @property
+    def pair(self):
+        try:
+            return self.re.pair+self.im.pair        
+        except:
+            return self.re,self.im
+    
+
+    def __str__(self):
+        return 'Complex'+str(self.pair)
+
+    @staticmethod
+    def zeros(order):
+        if order <= 0:
+            return 0
+        else:
+            return Complex(Complex.zeros(order-1),Complex.zeros(order-1))
 
     def __add__(self,other):
         try:
             return Complex(self.re+other.re,self.im+other.im)
         except:
-            return NotImplemented
+            try:
+                return Complex(*[a+b for a,b in zip_longest(self.pair,other.pair,fillvalue=0)])
+            except:
+                return NotImplemented
+            
+
+    def __radd__(self,other):
+        return self+other
 
     def __eq__(self,other):
         return self.im == other.im and self.re == other.re
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from complex import Complex
-
-a = Complex(Complex(1,2),Complex(3,4))
-b = Complex(3,4)
-
-print(b*a)
+a= Complex(1,2,3,4)
+b = Complex(5,3,4,5,6,7,7,7,6)
+print(b)
